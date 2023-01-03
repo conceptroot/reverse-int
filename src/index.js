18cf81736242ac38b00b3bbd6b74089e017578b2
conceptroot <conceptroot@gmail.com>
2023-01-03 20:07:53 +0530

    feat: realised reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n)
+    char_array = n.toString(10).split('')
+    reversed_string = char_array.reverse().join('')
+    return +reversed_string
 }
